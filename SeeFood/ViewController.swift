9fbbf54280688dc061aab713e7e4b821f98bf7e1
Bashir Yesufu <yesufubashir@gmail.com>
2022-01-31 09:24:00 +0100

    Getting Started

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by mac on 1/31/22.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
